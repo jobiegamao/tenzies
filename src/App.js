8eeb3f2d6288622fa54958cf8a35d2959b63ed23
Jobie Gamao <jobiemaygamao@gmail.com>
2022-04-20 17:29:51 +0800

    edit App, added listener

--- src/App.js
+++ src/App.js
@@ -6,6 +6,27 @@ import { nanoid } from 'nanoid'
 function App() {
 
   const [numsForDice, setNumsForDice ] = React.useState(generateDiceArray())
+  const [isGameFinish, setIsGameFinish] = React.useState(false)
+
+  //useEffect to follow numsForDice array change // listener for array
+  // if it is all the same number, isGameFinish === true
+
+  React.useEffect(() => {
+    // array.every(returns boolean) to check all array and returns boolean
+
+    //1st check if every die isSaved
+      //check every single die isSaved if true, if one is false, returns false
+    //if isAllSaved, get first value and check if values in array is the same
+    const isAllSaved = numsForDice.every( thisDie => (thisDie.isSaved) ) 
+    if(isAllSaved){
+      const checkArray = numsForDice.every((thisDie) => thisDie.number === numsForDice[0].number)
+      if(checkArray) {
+          setIsGameFinish(true)
+          console.log("Tenzies!")
+        }
+    }
+
+  }, [numsForDice]) //follows numsForDice
 
   const dieElements = numsForDice.map( num =>( 
      <Die 
@@ -17,8 +38,6 @@ function App() {
      />
   ))
 
-  //UPDATED: function for roll button
-  // must set new numsForDice except dice that are saved/pressed must stay
   function rollDice(){
     setNumsForDice( prev => prev.map( thisDie => {
       return thisDie.isSaved ? thisDie : generateDie_OneObj()
